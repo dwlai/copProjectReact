2a13069de21b94e158b3672a04aecb48163a19b6
davidlai <david.lai@evermight.com>
2017-03-11 22:44:34 -0500

    styled hat

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -37,35 +37,32 @@ export default class Home extends React.Component
 
     return(
  		<div>
- 			<div className="titleSection">
- 				<div className="titleContainer">
-		 				<p className="homeTitle">
-			    			Those who protect
-			    		</p>
- 						<img className="titleImage" src="./src/Components/security.png" />
- 				</div>
-    		</div>
+	 		<div className="titleSection">	
+			 				<p className="homeTitle">Those who protect</p>
+	    	</div>
 
     		<div className="container-fluid">
 	    		<div className="homeContent col-xs-12 col-sm-6 col-md-6 col-lg-6">
 	    		</div>
 	    		<div className="formContent col-xs-12 col-sm-6 col-md-6 col-lg-6">
 
-	    		<Form to={"officerPosts/"+ this.state.orgID +"/"+this.state.badge} method="POST" >
-	    			<label>Badge:<br/>
-	    				<input type="text" value={this.state.badge} name="badge" onChange={this.handleChange.bind(this)} />
-	    			</label>
-	    			<br/>
-	    			<label>
-			    			Find your police service:<br/>
-			    			<select name ="orgID" onChange={this.handleSelect.bind(this)}>
-			    				<option value="1">Toronto Police Service</option>
-			    				<option value="2">Peel Police</option>	    			
-			    			</select>
-		    		</label>
-	    			<br/>
-	    		<button type="submit">Go</button>
-	    		</Form>
+	    			{/*<img className="titleImage" src="./src/Components/security.png" />*/}
+	    			<img className="badgeCop" src="./src/Components/badge.png" />
+
+			    		<Form to={"officerPosts/"+ this.state.orgID +"/"+this.state.badge} method="POST" >
+			    			<label>
+			    				<input className="badgeInput" type="text" placeholder="enter badge" value={this.state.badge} name="badge" onChange={this.handleChange.bind(this)} />
+			    			</label>
+			    			<br/>
+			    			<label>
+					    			<select className="orgInput" name ="orgID" onChange={this.handleSelect.bind(this)}>
+					    				<option value="1">Toronto Police Service</option>
+					    				<option value="2">Peel Police</option>	    			
+					    			</select>
+				    		</label>
+			    			<br/>
+			    		<button className="badgeButton" type="submit">Go!</button>
+			    		</Form>
 	    		</div>
     		</div>
 
